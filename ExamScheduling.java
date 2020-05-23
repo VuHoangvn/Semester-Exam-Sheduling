c3c065683b0fa473a3e93998c7c4db48ac5d6682
unknown <nguyenvanhuyhnvh@gmail.com>
2020-05-23 14:54:11 +0700

    ham solve voi mo hinh 1 chay duoc r

--- ExamScheduling.java
+++ ExamScheduling.java
@@ -24,14 +24,138 @@ public class ExamScheduling {
 	private static void solve(String solverType) {
 		MPSolver solver = createSolver(solverType);
 		double infinity = MPSolver.infinity();
+
+		/** invariants */
+		int T = 6; // max time slot 
+		int S = 20; // number of subjects
+		int E = 23; // number of constraint two subjects
+		int R = 8; // number of rooms
+
+		Edge[] edges = { new Edge(0, 16), new Edge(1, 2), new Edge(1, 6), new Edge(1, 7), new Edge(1, 8),
+				new Edge(2, 11), new Edge(2, 16), new Edge(2, 17), new Edge(3, 14), new Edge(3, 16), new Edge(3, 17),
+				new Edge(4, 7), new Edge(4, 13), new Edge(4, 17), new Edge(5, 6), new Edge(5, 11), new Edge(6, 18),
+				new Edge(9, 12), new Edge(10, 13), new Edge(11, 17), new Edge(13, 15), new Edge(15, 17),
+				new Edge(16, 19) };
+		//  number of student in subject
+		int[] d = { 90, 66, 129, 81, 167, 176, 83, 109, 87, 126, 30, 107, 67, 58, 49, 133, 41, 94, 150, 87 };
+		// max student in room
+		int[] c = { 193, 95, 195, 39, 172, 173, 29, 53 };
+
+		/** variables */
+		// x[s][t] = 1 when subject s is organized in the t(th) exam
+		MPVariable[][] x = new MPVariable[S][T];
+		for (Integer s = 0; s < S; s++) {
+			x[s] = solver.makeBoolVarArray(T);
+		}
+		// y[s][r] = 1 when subject s is organized in room r
+		MPVariable[][] y = new MPVariable[S][R];
+		for (Integer s = 0; s < S; s++) {
+			y[s] = solver.makeBoolVarArray(R);
+		}
+		// z[t] = 1 when time slot t is used
+		MPVariable[] z = solver.makeBoolVarArray(T);
+		// minimize number of time slots are used
+		MPObjective obj = solver.objective();
+		for (MPVariable objVar : z) {
+			obj.setCoefficient(objVar, 1);
+		}
+
+		/** add constraint 1 */
+		//SUMt (x[s][t]) = 1 
+		MPConstraint[] constraint1 = new MPConstraint[S];
+		for (int s = 0; s < S; s++) {
+			constraint1[s] = solver.makeConstraint(1, 1);
+			for (int t = 0; t < T; t++) {
+				constraint1[s].setCoefficient(x[s][t], 1);
+			}
+		}
+
+		/** add constraint 2 */
+		//SUMr (y[s][r]*c[r] >= d[s]
+		MPConstraint[] constraint2 = new MPConstraint[S];
+		for (int s = 0; s < S; s++) {
+			constraint2[s] = solver.makeConstraint(d[s], infinity);
+			for (int r = 0; r < R; r++) {
+				constraint2[s].setCoefficient(y[s][r], c[r]);
+			}
+		}
+
+		/** add constraint 3 */
+		// x[s1][t] + s[s2][t] <= z[t] <=> x[s1][t] + x[s2][t] <= 1 and z[t] >= x[s][t
+		MPConstraint[][] constraint3 = new MPConstraint[E][T];
+		for (int i = 0; i < E; i++) {
+			for (int t = 0; t < T; t++) {
+				constraint3[i][t] = solver.makeConstraint(-infinity, 0);
+				constraint3[i][t].setCoefficient(x[edges[i].a][t], 1);
+				constraint3[i][t].setCoefficient(x[edges[i].b][t], 1);
+				constraint3[i][t].setCoefficient(z[t], -1);
+			}
+		}
+
+		/** add constraint 4 */
+		// x[s1][t] + x[s2][t] = 2 => y[s1][r] + y[s2][r] <= 1
+		int bigM = 50;
+		MPConstraint[][][][] constraint4 = new MPConstraint[S][S][T][R];
+		for (int s1 = 0; s1 < S; s1++) {
+			for (int s2 = s1 + 1; s2 < S; s2++) {
+				for (int t = 0; t < T; t++) {
+					for (int r = 0; r < R; r++) {
+						if (s1 != s2) {
+							constraint4[s1][s2][t][r] = solver.makeConstraint(0, 2 * bigM + 1);
+							constraint4[s1][s2][t][r].setCoefficient(x[s1][t], bigM);
+							constraint4[s1][s2][t][r].setCoefficient(x[s2][t], bigM);
+							constraint4[s1][s2][t][r].setCoefficient(y[s1][r], 1);
+							constraint4[s1][s2][t][r].setCoefficient(y[s2][r], 1);
+						} else {
+							constraint4[s1][s2][t][r] = solver.makeConstraint(0, 1);
+							constraint4[s1][s2][t][r].setCoefficient(x[0][0], 0);
+						}
+					}
+				}
+			}
+		}
+
+		/** Minimize by default */
+		final MPSolver.ResultStatus resultStatus = solver.solve();
+
+		/** printing */
+		if (resultStatus != MPSolver.ResultStatus.OPTIMAL) {
+			System.err.println("The problem does not have an optimal solution!");
+			return;
+		} else {
+			System.out.print("ket qua:" + "\n");
+			for (int s = 0; s < S; s++) {
+				System.out.print("subject: " + s);
+				for (int t = 0; t < T; t++) {
+					if (x[s][t].solutionValue() > 0) {
+						System.out.print(" time slot: " + t);
+						break;
+					}
+				}
+				System.out.print("room: ");
+				for (int r = 0; r< R; r++) {
+					if (y[s][r].solutionValue() > 0) {
+						System.out.print(r + "  ");
+					}
+				}
+				System.out.print("\n");
+				
+			}
+		}
+		return;
+	}
+	
+	private static void solve2(String solverType) {
+		MPSolver solver = createSolver(solverType);
+		double infinity = MPSolver.infinity();
 		// subject number
-		int N = 20;
+		int S = 20;
 		// room number
-		int M = 8;
+		int R = 8;
 		// edge constraint
 		int E = 23;
-		// max session
-		int K = 100;
+		// max time slot
+		int T = 100;
 		// edge constraint
 		Edge[] edge = {
 			new Edge(0, 16),
@@ -62,119 +186,129 @@ public class ExamScheduling {
 		int[] d = {90, 66, 129, 81, 167, 176, 83, 109, 87, 126, 30, 107, 67, 58, 49, 133, 41, 94, 150, 87};
 		//max student in room
 		int[] c = {193, 95, 195, 39, 172, 173, 29, 53};
-		MPVariable[][] x = new MPVariable[N][K];
-		MPVariable[][] y = new MPVariable[N][M];
-		MPVariable[] z = new MPVariable[K];
-		for (int i = 0; i < N; i++) {
-			for(int j=0;j<K;j++) {
-				x[i][j] = solver.makeIntVar(0, 1, "x["+i+"]["+j+"]");
-			}
-			for(int j=0;j<M;j++) {
-				y[i][j] = solver.makeIntVar(0, 1, "y["+i+"]["+j+"]");
-			}
-		}
-		for (int i = 0; i < K; i++) {
-			z[i]= solver.makeIntVar(0,1,"z["+i+"]");
-		}
-		// add constraint 1
-		MPConstraint[] constraint1 = new MPConstraint[N];
-		for (int i = 0; i < N; i++) {
-			constraint1[i] = solver.makeConstraint(1, 1);
-			for (int j = 0; j < K; j++) {
-				constraint1[i].setCoefficient(x[i][j], 1);
-			}
-		}
-		//add constraint 2
-		MPConstraint[] constraint2 = new MPConstraint[N];
-		for (int i = 0; i < N; i++) {
-			constraint2[i] = solver.makeConstraint(d[i], infinity);
-			for (int j = 0; j < M; j++) {
-				constraint2[i].setCoefficient(y[i][j], c[j]);
-			}
-		}
-		//add constraint 3
-		MPConstraint[][] constraint3 = new MPConstraint[E][K];
-		for (int i = 0; i < E; i++) {
-			for (int j = 0; j < K; j++) {
-				constraint3[i][j] = solver.makeConstraint(-infinity, 0);
-				constraint3[i][j].setCoefficient(x[edge[i].a][j], 1);
-				constraint3[i][j].setCoefficient(x[edge[i].b][j], 1);
-				constraint3[i][j].setCoefficient(z[j], -1);
-			}
-		}
-		//add constraint 4
-		MPConstraint[][][][] constraint4 = new MPConstraint[N][N][K][M];
-		for (int i1 = 0; i1 < N-1; i1++) {
-			for(int i2 = i1+1; i2 < N;i2++) {
-				for(int j=0;j<K;j++) {
-					for (int k = 0; k < M; k++) {
-						constraint4[i1][i2][j][k] = solver.makeConstraint(-infinity, 2 * infinity + 1);
-						constraint4[i1][i2][j][k].setCoefficient(x[i1][j], infinity);
-						constraint4[i1][i2][j][k].setCoefficient(x[i2][j], infinity);
-						constraint4[i1][i2][j][k].setCoefficient(y[i1][k], 1);
-						constraint4[i1][i2][j][k].setCoefficient(y[i2][k], 1);
-					}
+
+		MPVariable[][][] X = new MPVariable[S][T][R];
+		MPVariable[][] Y = new MPVariable[S][T];
+		MPVariable[] Z = new MPVariable[T];
+		//add var x
+		for(int s=0; s<S;s++) {
+			for(int t=0;t<T;t++) {
+				for(int r= 0; r<R;r++) {
+					X[s][t][r] = solver.makeIntVar(0, 1, "x["+s+"]["+t+"]["+r+"]");
 				}
 			}
 		}
-
-		//add objective
-		MPObjective obj = solver.objective();
-		for (MPVariable objVar : z) {
-			obj.setCoefficient(objVar, 1);
+		//add var y
+		for(int s=0;s<S;s++){
+			for(int t=0;t<T;t++) {
+				Y[s][t] = solver.makeIntVar(0,1,"y["+s+"]["+t+"]");
+			}
 		}
-//		MPObjective obj2 = solver.objective();
-//		for (int i = 0; i < N; i++) {
-//			for (int j = 0; j < K; j++) {
-//				obj2.setCoefficient(y[i][j], 1);
-//			}
-//		}
+		// add var z
+		for(int t=0;t<T;t++) {
+			Z[t] = solver.makeIntVar(0, 1, "x["+t+"]");
+		}
+		// add constraint1
+		MPConstraint[] constraint1 = new MPConstraint[S];
+		for(int s=0; s<S;s++) {
+			constraint1[s] = solver.makeConstraint(1,1);
+			for (int t=0;t<T;t++) {
+				constraint1[s].setCoefficient(Y[s][t], 1);
+			}
+		}
+		// add constraint2
+		MPConstraint[][] constraint2 = new MPConstraint[E][T];
+		for (int i=0;i<edge.length;i++) {
+			for(int t=0;t<T;t++) {
+				constraint2[i][t] = solver.makeConstraint(-1, 0);
+				constraint2[i][t].setCoefficient(Y[edge[i].a][t], 1);
+				constraint2[i][t].setCoefficient(Y[edge[i].b][t], 1);
+				constraint2[i][t].setCoefficient(Z[t], -1);
+			}
+		}
+		// add constraint3
+		MPConstraint[][] constraint3 = new MPConstraint[T][R];
+		for (int t = 0; t < T; t++) {
+			for (int r = 0; r < R; r++) {
+				constraint3[t][r] = solver.makeConstraint(0, 1);
+				for (int s = 0; s < S; s++) {
+					constraint3[t][r].setCoefficient(X[s][t][r],1);
+				}
+			}
+		}
+		//add constraint4
+		MPConstraint[][][] constraint4 = new MPConstraint[S][T][R];
+		for(int s=0;s<S;s++) {
+			for(int t=0;t<T;t++) {
+				for(int r=0;r<R;r++) {
+					constraint4[s][t][r] = solver.makeConstraint(0, 1);
+					constraint4[s][t][r].setCoefficient(Y[s][t], 1);
+					constraint4[s][t][r].setCoefficient(X[s][t][r], -1);
+ 				}
+			}
+		}
+		// add constraint5
+		MPConstraint[][] constraint5 = new MPConstraint[S][T];
+		for(int s=0;s<S;s++) {
+			for(int t=0;t<T;t++) {
+				constraint5[s][t] = solver.makeConstraint(0, infinity);
+				for(int r=0;r<R;r++) {
+					constraint5[s][t].setCoefficient(X[s][t][r], c[r]);
+				}
+				constraint5[s][t].setCoefficient(Y[s][t], -d[s]);
+			}
+		}
+ 		//add objective
+		MPObjective obj = solver.objective();
+		for( MPVariable v : Z) {
+			obj.setCoefficient(v, 1);
+		}
 		obj.setMinimization();
-//		obj2.setMinimization();
 		final MPSolver.ResultStatus resultStatus = solver.solve();
 		if (resultStatus != MPSolver.ResultStatus.ABNORMAL) {
 			System.err.println("The problem does not have an optimal solution!");
 			return;
 		} else {
 			System.out.print("ket qua:" + "\n");
-			for (int i = 0; i < N; i++) {
-				System.out.print("subject: " + i);
-				for (int j = 0; j < K; j++) {
-					if (x[i][j].solutionValue() > 0) {
-						System.out.print(" session: " + j);
+			for (int s = 0; s < S; s++) {
+				System.out.print("subject: " + s);
+				for (int t = 0; t < T; t++) {
+					if (Y[s][t].solutionValue() > 0) {
+						System.out.print(" time slot: " + t);
+						System.out.print("room: ");
+						for (int r = 0; r< R; r++) {
+							if (X[s][t][r].solutionValue() > 0) {
+								System.out.print(r + "  ");
+							}
+						}
+						System.out.print("\n");
 						break;
 					}
 				}
-				System.out.print("room: ");
-				for (int j = 0; j < M; j++) {
-					if (y[i][j].solutionValue() > 0) {
-						System.out.print(j + "  ");
-					}
-				}
-				System.out.print("\n");
+				
 			}
 		}
 		return;
 	}
 
 	public static void main(String[] args) {
-//		try {
-//			System.out.println("---- Integer programming example with SCIP (recommended) ----");
-//			solve("SCIP_MIXED_INTEGER_PROGRAMMING");
-//		} catch (java.lang.IllegalArgumentException e) {
-//			System.err.println("Bad solver type: " + e);
-//		}
+		try {
+			System.out.println("---- Integer programming example with SCIP (recommended) ----");
+			solve("SCIP_MIXED_INTEGER_PROGRAMMING");
+		} catch (java.lang.IllegalArgumentException e) {
+			System.err.println("Bad solver type: " + e);
+		}
 		try {
 			System.out.println("---- Integer programming example with CBC ----");
 			solve("CBC_MIXED_INTEGER_PROGRAMMING");
 		} catch (java.lang.IllegalArgumentException e) {
 			System.err.println("Bad solver type: " + e);
 		}
-//		try {
-//			System.out.println("---- Integer programming example with GLPK ----");
-//			solve("GLPK_MIXED_INTEGER_PROGRAMMING");
-//		} catch (java.lang.IllegalArgumentException e) {
-//			System.err.println("Bad solver type: " + e);
-//		}
+		try {
+			System.out.println("---- Integer programming example with GLPK ----");
+			solve("GLPK_MIXED_INTEGER_PROGRAMMING");
+		} catch (java.lang.IllegalArgumentException e) {
+			System.err.println("Bad solver type: " + e);
+		}
 	}
 }
